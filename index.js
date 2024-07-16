f4c2535b148ef9cc44589d0a86413fe36d89e6ca
sarahpimentelmedeiros <sarahpimentelmedeiros@gmail.com>
2024-07-16 20:57:53 -0300

    add verificação de login

---
+++ index.js
@@ -0,0 +1,13 @@
+function logar(){
+    var login = document.getElementById('ilogin').value;
+    var senha = document.getElementById('isenha').value;
+
+    if(login == 'admin' && senha == '12345678'){
+        location.href = 'home.html';
+    }else{
+        alert('Usuário ou senha incorreto. Tente novamente!');
+    }
+}
+function cadastrar(){
+    location.href = 'form-login.html';
+}
